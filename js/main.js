c7aa2a410bccf773c38795b3e403c8bb244fdbde
Tuiaara87 <77272499+Tuiaara87@users.noreply.github.com>
2021-10-06 16:58:39 +0300

    Организация модуля. Правки

--- js/main.js
+++ js/main.js
@@ -1,70 +1,8 @@
-// Возвращение случайного числа
-
-function getNumber(from, to) {
-  const rand = from + Math.random() * (to + 1 - from);
-  return Math.ceil(rand);
-}
-
-// Генерация данных
-
-
-const DESCRIPTIONS = [
-  'музыка исцеляет мою душу',
-  'люблю порядок во всем',
-  'дорогу осилит идущий',
-  'просто хороший день',
-];
-
-const NAMES = [
-  'Саша',
-  'Маша',
-  'Шура',
-  'Наташа',
-];
-
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-
-const createMyFirstObject = (id) => {
-  const randomDescriptionIndex = getNumber(0, DESCRIPTIONS.length - 1);
-  const randomLikesCount = getNumber(15, 200);
-  const comments = [];
-  const randomQuantityComments = getNumber(1, 5);
-
-  for (let index = 0; index < randomQuantityComments; index++) {
-    const randomCommentsId = getNumber(0, 200);
-    const randomAvatarIndex = getNumber(1, 6);
-    const randomMessagesIndex = getNumber(0, MESSAGES.length - 1);
-    const randomNamesIndex = getNumber(0, NAMES.length - 1);
-    comments.push({
-      id: randomCommentsId,
-      avatar: `img/avatar-${randomAvatarIndex}.svg`,
-      message: MESSAGES[randomMessagesIndex],
-      name: NAMES[randomNamesIndex],
-    });
-  }
-
-  return {
-    id: id,
-    url: `photos/${id}.jpg`,
-    avatar: `img/avatar-${id}.svg`,
-    description: DESCRIPTIONS[randomDescriptionIndex],
-    likes: randomLikesCount,
-    comments: comments,
-  };
-};
-
-const photos = [];
-for (let index = 1; index <= 25; index++) {
-  const photo = createMyFirstObject(index);
-  photos.push(photo);
-}
-
-
+import './util.js';
+import './setup.js';
+import './download-photo.js';
+import './send.js';
+import './prewatch.js';
+import './filter.js';
+import './data.js';
+import './script.js';
--- js/script.js
+++
@@ -1,7 +0,0 @@
-import './main.js';
-import './util.js';
-import './setup.js';
-import './download-photo.js';
-import './send.js';
-import './prewatch.js';
-import './filter.js';
