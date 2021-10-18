44d36d6db4dd38d2de5efa78851a2abbb80609ad
Tuiaara87 <77272499+Tuiaara87@users.noreply.github.com>
2021-10-18 15:28:54 +0300

    Правки отрисуй меня часть 1

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -2,7 +2,7 @@ import { getPhotos } from './data.js';
 
 const photos = getPhotos();
 
-const containerPhotos = document.querySelector('#container-photos');
+const containerPhotos = document.querySelector('.pictures');
 
 const templateWrapper = document.querySelector('#picture').content; // Находим фрагмент с содержимым темплейта
 
@@ -12,9 +12,12 @@ const fragment = document.createDocumentFragment();
 
 photos.forEach((photo) => {
   const a = template.cloneNode(true); // Клонируем элемент со всеми "внутренностями"
-  a.children[0].src = photo.url;
-  a.children[1].children[0].textContent = photo.comments.length; // Записываем содержимое
-  a.children[1].children[1].textContent = photo.likes;
+  a.querySelector('.picture__img').src = photo.url;
+  a.querySelector('.picture__comments').textContent = photo.comments.length;
+  a.querySelector('.picture__likes').textContent = photo.likes;
+  // a.children[0].src = photo.url;
+  // a.children[1].children[0].textContent = photo.comments.length; // Записываем содержимое
+  // a.children[1].children[1].textContent = photo.likes;
   fragment.appendChild(a);
 });
 
